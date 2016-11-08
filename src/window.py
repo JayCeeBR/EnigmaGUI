4846f98626dd8c515956abf2740d9d85490832b5
Wilson Moura <wilsonmoura10@yahoo.com>
2016-11-08 13:36:21 -0200

    Window Menu add

--- src/window.py
+++ src/window.py
@@ -9,13 +9,28 @@ class Window(object):
 		self.window.minsize(800,600)
 		self.window.maxsize(800,600)
 		self.window.title("Enigma")
-		self.plugboard = Frame()
-		self.plugboard.pack(side=TOP)
 
-		self.label1 = Label(self.plugboard, text="Plugboard")
-		self.label1.pack(side=LEFT)
+		self.menubar = Menu(self.window)
+		self.settings = Menu(self.menubar, tearoff=0)
+		self.menubar.add_cascade(label="Menu", menu=self.settings)
+		self.settings.add_command(label="Configure Rotors", command=self.create_rotors)
+		self.settings.add_command(label="Configure Plugboard", command=self.create_plugboard)
+		self.settings.add_command(label="Configure Reflector", command=self.create_reflector)
+		self.settings.add_separator()
+		self.settings.add_command(label="Exit", command=self.destroy)
 
+
+		self.window.config(menu=self.menubar)
 		self.window.mainloop()
+
+	def create_rotors(self):
+		pass
+
+	def create_plugboard(self):
+		pass
+
+	def create_reflector(self):
+		pass
 
 	def destroy(self):
 		self.window.destroy()
